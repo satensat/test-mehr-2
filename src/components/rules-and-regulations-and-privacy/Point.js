9a7ea2956625060a1e1e7af3b6e2707a165d9af8
Saten Shahijani <satenikshahi2020@gmail.com>
2024-03-16 17:27:52 +0330

    changed

--- src/components/rules-and-regulations-and-privacy/Point.js
+++ src/components/rules-and-regulations-and-privacy/Point.js
@@ -1,15 +1,20 @@
 import React from "react";
 
-export default function Point({title,content}) {
+export default function Point({ title, content }) {
   return (
     <div className="flex flex-col   rounded-3xl ">
-      <div className="bg-[#13625C] px-3 py-2 rounded-t-xl text-[#FFFFFF] text-sm leading-6 font-bold">{title}</div>
-      <div className=" flex flex-col gap-3  bg-[#F7F7F7] p-3  rounded-b-3xl text-[#525252] text-sm leading-6 text-justify ">
-        {content.map((item,index)=>
-        <p key={index}>
-            {item}
-        </p>)}
+      <div className="bg-[#13625C] px-3 py-2 rounded-t-xl text-[#FFFFFF] text-sm leading-6 font-bold">
+        {title}
+      </div>
+      <div className=" flex flex-col  bg-[#F7F7F7] p-3  rounded-b-3xl text-[#525252] text-sm leading-6 text-justify ">
+        {content.map((item, index) => (
+             <p className={item.classSetted} key={index}>
+             {item.content}
+           </p>
+        ))}
       </div>
     </div>
   );
 }
+
+
